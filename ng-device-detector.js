9fb618fad2346cab235d3a8a0f97603f9ab56979
srfrnk <srfrnk@gmail.com>
2014-05-26 16:45:10 +0300

    created JS and docs

---
+++ ng-device-detector.js
@@ -0,0 +1,59 @@
+(function(angular) {
+"use strict";
+angular.module("ng.deviceDetector")
+.factory("deviceDetector", [
+	function () {
+		var ua=navigator.userAgent;
+		var deviceInfo = {
+			userAgent:ua,
+			raw:{
+				os:{
+						windows:/\bWindows\b/.test(ua),
+						mac:/\bMacOS\b/.test(ua),
+						linux:/\bLinux\b/.test(ua),
+						unix:/\bUNIX\b/.test(ua),
+						android:/\bAndroid\b/.test(ua),
+				},
+				browser:{
+					chrome:/\bChrome\b/.test(ua),
+					firefox:/\Firefox\b/.test(ua),
+					safari:/\Safari\b/.test(ua),
+					opera:/\Opera\b/.test(ua),
+					ie:/\bMSIE\b/.test(ua) || /\Trident\b/.test(ua),
+				},
+				device:{
+					android:/\bAndroid\b/.test(ua),
+					ipad:/\biPad\b/.test(ua),
+					iphone:/\biPhone\b/.test(ua),
+					blackberry:/\bblackberry\b/.test(ua),
+				}
+			}
+			os: deviceInfo.raw.os.windows ? "windows" :
+					deviceInfo.raw.os.mac ? "mac" :
+						deviceInfo.raw.os.linux ? "linux" :
+							deviceInfo.raw.os.unix? "unix" :
+								deviceInfo.raw.os.android? "android",
+			browser: deviceInfo.raw.browser.ie ? "ie" :
+					deviceInfo.raw.browser.opera ? "opera" :
+						deviceInfo.raw.browser.safari ? "safari" :
+							deviceInfo.raw.browser.firefox? "firefox" :
+								deviceInfo.raw.browser.chrome? "chrome",
+			device: deviceInfo.raw.device.android ? "android" :
+						deviceInfo.raw.device.iphone ? "iphone" :
+							deviceInfo.raw.device.ipad ? "ipad" :
+								deviceInfo.raw.device.blackberry ? "blackberry"
+		};
+		return deviceInfo;
+	}
+])
+.directive('deviceDetector', ["deviceDetector",function (deviceDetector) {
+	return {
+		restrict: "A",
+		link: function (scope, elm, attrs) {
+			elm.addClass('os-'+deviceDetector.os);
+			elm.addClass('browser-'+deviceDetector.browser);
+			elm.addClass('device-'+deviceDetector.device);
+		}
+	};
+}]);
+})(angular);
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
-#Angular module to detect OS / Browser / Device
+#ng-device-detector
+##Angular module to detect OS / Browser / Device
+
+Uses user-agent to set classes or directly usable with JS.
+
+### Install
+* Run $ bower install ng-device-detector --save
+* Add script load to HTML: <script type="text/javascript" src=".../ng-device-detector.js"></script>
+* Add module to your app dependencies: ...angular.module("...", [..."ng.deviceDetector"...])...
+* To add classes <div ... ng-device-detector ...>
+* To use directly add "deviceDetector" service to your injectors...
+
+### deviceDetector service
+Hold the following properties for usage:
+* raw : object : contains the raw values... for internal use mostly.
+* os : string : name of current OS
+* browser : string : name of current browser
+* device : string : name of current device
