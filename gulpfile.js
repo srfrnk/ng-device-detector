396ecb1fd387335d281cbb3621a16300c03c3c70
srfrnk <srfrnk@gmail.com>
2014-12-28 11:10:34 +0200

    Fixes #15,#16

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var karma = require('karma').server;
+
+gulp.task('default', ["minify","test"]);
+
+gulp.task('minify', function() {
+  gulp.src('ng-device-detector.js')
+    .pipe(uglify())
+    .pipe(concat("ng-device-detector.min.js"))
+    .pipe(gulp.dest('.'))
+});
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
